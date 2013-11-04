583e9047d3aad1bcafca273434292f7fa01078bf
Erik Thorselius <erik.thorslius@gmail.com>
2013-11-04 19:37:10 +0100

    Lets symlink check if file exists.

--- symlink.rb
+++ symlink.rb
@@ -8,5 +8,6 @@ Dir['*'].each do |file|
   next if file =~ /install/
   target = File.join(home, ".#{file}")
   next if File.symlink? target
+  next if File.exists? target
   `ln -s #{File.expand_path file} #{target}`
 end
